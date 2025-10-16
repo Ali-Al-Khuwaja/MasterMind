9fa74deb9f145d15a582070b618688d341d173b6
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2025-10-16 20:50:53 +0300

    Add a longer comment

--- lib/colors_module.rb
+++ lib/colors_module.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# ColorsModule is responsible for storing a hash with different colors and accompanied with false booleans.
-# The purpose of the booleans is to track the status
+# ColorsModule is responsible for storing a hash with different colors with false booleans by default.
+# The purpose of the booleans is to track the status, if it's true , then it was used .
 module ColorsModule
-  # A class variable, four colors only (for now)
+  # A class variable, four colors only (for now), you sure ali ?? this os a module , check the duck
   COLORS_HASH = { red: false, yellow: false, blue: false, green: false } # rubocop:disable Style/MutableConstant
   def self.print_colors
     puts ColorsModule::COLORS_HASH.keys
