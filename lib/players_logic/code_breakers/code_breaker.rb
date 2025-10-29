4559bc798dcc597dc34dac9e6534754712b265b6
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2025-10-29 14:48:33 +0300

    codeBreaker now extends from character object.  Relocate where and how we get the name to the character object . Improved error handling by separating them into multiple methods for cleaner code and acceptable for the cops. Fixed the logic for validating inputs.

--- lib/players_logic/code_breakers/code_breaker.rb
+++ lib/players_logic/code_breakers/code_breaker.rb
@@ -5,60 +5,66 @@ require_relative '../../colors_module'
 require 'pry-byebug'
 
 # This is a class responsible for managing the code breaker role
-class CodeBreaker
+class CodeBreaker < Character
   include ColorsModule
 
-  def initialize
-    @player = Character.new
-    @player.add_name
-    # Make an array called 'indicator' that stores 4 elements inside it, each element will
-    # be a boolean with a false value .
-    # When the breaker picks and index it'll signal to the indicator array to turn on
-    # the corresponding index in the 'indicator' array to act like a form of verification if the
-    # index has been used before
+  def initialize(name = nil)
+    super
     @index_is_used = Array.new(4, false)
-    @guess_hash = {} # empty for now
+    @guess_hash = {}
   end
 
-  def check_index(round, index)
-    p round, index
-    binding.pry
-    if @index_is_used[round] == true || !index.between?(1, 4) || @guess_hash.value?(index)
-      puts 'Try another index, you have used this index before or this index is not between 1 to 4'
-      index = gets.chomp.to_i
-      check_index(round, index)
-    elsif @index_is_used[round] == false && index.between?(1, 4)
-      @index_is_used[round] = true
-      # @guess_hash.store(color, index)
-      { index: true }.keys.first
+  def check_index(index)
+    # binding.pry
+    if @index_is_used[index]
+      puts "Try another index, you have used this index: #{index} before "
+      ask_for_index
+    elsif !index.between?(0, 3)
+      puts "Please pick an index from 0 to 3 ,You picked #{index}"
+      ask_for_index
+    elsif !@index_is_used[index] && index.between?(0, 3)
+      @index_is_used[index] = true
+      index
+    end
+  end
+
+  def ask_for_index
+    # binding.pry
+    puts 'pick an index from 0 to 3'
+    index = gets.chomp.to_i
+    check_index(index)
+  end
+
+  def ask_for_color(round)
+    # binding.pry
+    puts "Pick the #{round + 1} random color from the list below "
+    puts ColorsModule.print_colors
+    color = gets.chomp.to_sym
+    validate_color(color, round)
+  end
+
+  def validate_color(color, round)
+    # binding.pry
+    if ColorsModule::COLORS_HASH.key?(color) && !@guess_hash.include?(color)
+      color
+    elsif !ColorsModule::COLORS_HASH.key?(color)
+      puts "This color #{color} , is not a color bro"
+      ask_for_color(round)
+    elsif @guess_hash.include?(color)
+      puts "You have used this color:#{color} before !"
+      ask_for_color(round)
     end
   end
 
   def guess_code
     4.times do |round|
-      # a boolean for checking if the color is already present in guess_hash
-      puts "Enter the #{round + 1} random color from the list below"
-      puts ColorsModule.print_colors
-      color = gets.chomp.to_sym
-      color_is_used = @guess_hash.key?(color) # Check if the color has been chosen before
-      puts 'pick an index please'
-      index = gets.chomp.to_i
-      is_valid = check_index(round, index)
-      until ColorsModule::COLORS_HASH.key?(color) && color_is_used == false && is_valid
-        # p ColorsModule::COLORS_HASH.key?(color)
-        # p color && color_is_used
-        puts "Wrong ! The color #{color} is used or you haven't picked a color that is from the list, pick another please."
-        puts ColorsModule.print_colors
-        color = gets.chomp.to_sym
-        color_is_used = @guess_hash.key?(color)
-        binding.pry
-        is_valid ||= check_index(round, index)
-      end
+      color = ask_for_color(round)
+      index = ask_for_index
       @guess_hash.store(color, index)
-      p @guess_hash
     end
   end
 end
+
 # This object will set the name of player and the guessed code
-test = CodeBreaker.new
+test = CodeBreaker.new('ali')
 test.guess_code
