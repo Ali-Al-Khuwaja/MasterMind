c5980e021bfcafaeb7ea98fe8ca55eedd538e963
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2026-02-04 22:53:20 +0300

    update main.rb

---
+++ docs/pseudo/main.md
@@ -0,0 +1,55 @@
+IMPORT 'lib/game'
+
+CLASS Main
+
+    METHOD init
+        game = Game.new
+    END
+
+    METHOD start_menu
+        PRINT "Select an option number"
+        PRINT '1. Start game'
+        PRINT '2. Stop game'
+        answer = get.input
+        start_menu_actions(answer)
+    END
+
+    METHOD validate(answer)
+        IF answer IS NOT null AND IS Number
+    END
+
+    METHOD start_menu_actions(answer)
+        IF answer IS valid
+            IF answer IS "1"
+                roles_menu
+            ELSE answer IS "2"
+                stop_game
+            END
+        END
+    ENS
+
+    METHOD roles_menu
+        PRINT "choose the type of you opponent for your game"
+        PRINT "1. human vs human"
+        PRINT "2, human vs computer"
+        answer = get.input
+    END
+
+    METHOD roles_menu_actions
+        IF answer IS valid
+            IF answer IS "1"
+                human_vs_human
+            ELSE answer IS "2"
+                human_vs_computer
+            END
+        END
+    END
+
+    METHOD difficulty_menu
+        PRINT 'Choose the difficulty of your game'
+        PRINT '1. Easy mode'
+        PRINT '2. Hard mode'
+        answer = get.input
+    END
+
+END
--- main.rb
+++ main.rb
@@ -2,4 +2,88 @@
 
 require_relative 'lib/game'
 
-start_game
+# The main class, it works as the coordinator
+class Main
+  def initialize
+    game = Game.new
+    start_menu
+  end
+
+  def start_menu
+    type_of_menu = 'start'
+    puts 'Select an option by number...'
+    sleep 0.4
+    puts '1. Start game'
+    puts '2. Stop game'
+    answer = gets.chomp.to_i
+    validate(answer, type_of_menu)
+  end
+
+  def start_menu_actions(answer)
+    if answer == 1
+      roles_menu
+    elsif answer == 2
+      puts 'Stopping game'
+      stop_game
+    end
+  end
+
+  def stop_game
+    exit(0)
+  end
+
+  def roles_menu
+    type_of_menu = 'roles'
+    puts 'Choose the type of your opponent for your game'
+    sleep 0.4
+    puts '1. Human vs Human'
+    puts '2. Human vs Computer'
+    answer = gets.chomp.to_i
+    validate(answer, type_of_menu)
+  end
+
+  # def roles_menu_actions
+  #   if answer == 1
+  #     human_vs_human
+  #   elsif answer == 2
+  #     human_vs_computer
+  #   end
+  # end
+
+  # def difficulty_menu
+  #   type_of_menu = 'difficulty'
+  #   puts 'Choose the difficulty of your game'
+  #   sleep 0.4
+  #   puts '1. Easy mode'
+  #   puts '2. Hard mode'
+  #   answer = gets.chomp.to_i
+  #   validate(answer, type_of_menu)
+  # end
+
+  # def difficulty_menu_actions(answer)
+  #   if answer == 1
+  #     easy_mode
+  #   elsif answer == 2
+  #     hard_mode
+  #   end
+  # end
+
+  def validate(answer, type_of_menu)
+    raise ArgumentError, 'bad argument, only numbers are accepted' unless !answer.nil? && answer.is_a?(Integer)
+
+    until answer.between?(1, 2)
+      puts 'Enter a valid answer, pick 1 or 2 from the shown options' unless answer.between?(1, 2)
+      answer = gets.chomp.to_i
+    end
+    case type_of_menu
+    when 'start'
+      start_menu_actions(answer)
+      # when 'roles'
+      #   roles_menu_actions(answer)
+      # when 'difficulty'
+      #   difficulty_menu_actions(answer)
+    end
+  end
+end
+
+test = Main.new
