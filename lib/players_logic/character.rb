21ea57980d5fa74f7e03319e7969527a9a78c1aa
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2026-02-04 22:53:05 +0300

    update character.rb

--- lib/players_logic/character.rb
+++ lib/players_logic/character.rb
@@ -5,13 +5,15 @@ class Character
   attr_reader :name
 
   def initialize(name = nil)
-    # if it was not provided ,ask for it .
-    if name.nil? || name.strip.empty?
-      puts 'What is your name ?'
-      @name = gets.chomp
-    else
-      @name = name
+    @name = name
+    loop do
+      if name.nil? || name.strip.empty?
+        puts 'What is your name ?'
+        @name = gets.chomp
+      end
+      break if @name.is_a?(String) && !@name.strip.empty?
+
+      puts 'Name required, Please enter a valid name.'
     end
-    raise ArgumentError 'name required' if @name.nil? || @name.strip.empty?
   end
 end
