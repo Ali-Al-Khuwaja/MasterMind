432d69ce5dd2a90ce8de89fa74e070cc567230ff
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2026-02-23 14:15:21 +0300

    add showscore method

--- lib/score.rb
+++ lib/score.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 # This class is responsible for adding score points to a winning player.
 #
 # #add_point method
@@ -13,9 +14,20 @@ class Score
 
   def increase_maker_score(amount)
     @code_maker_score += amount
+    show_score('maker')
   end
 
   def increase_breaker_score(amount)
     @code_breaker_score += amount
+    show_score('breaker')
+  end
+
+  def show_score(who)
+    binding.pry
+    if who == 'maker'
+      puts "code_maker's score :#{@code_maker_score}"
+    elsif who == 'breaker'
+      puts "code_breaker's score:#{@code_breaker_score}"
+    end
   end
 end
